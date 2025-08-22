7c88b4346fc44d5acf500d8ebf9f3b6366a891e8
Potatoccc <18991370571@163.com>
2025-08-22 23:37:34 +0800

    util

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,20 +80,3 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
-
-uint64 
-kcount(void)
-{
-  struct run *r;
-
-  uint64 count = 0;
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  while(r) {
-     count += PGSIZE;
-     r = r -> next;
-  }
-  release(&kmem.lock);
-
-  return count;
-}
